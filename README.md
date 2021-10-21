4d89da20f24a1cfd87c08387529af1cd1eb61e5d
taka61 <taka.tennisloveup61@gmail.com>
2021-10-21 17:25:17 +0900

    READ.mdを編集

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # emotional_awareness
+You can be aware of your feelings quickly to use the “emotional_awareness”.
+It is like a super simple app that records your emotions.
+We are often asked someone ‘how are you?’, and almost people say ‘I’m good or fine’.
+However, Does it really mean?
+You can choose your real feelings through this npm, and it gives you a word that is written by English or Japanese according to your emotions.
+I chose those words for me, so those may not suit you. Although I hope you enjoy it👋
+
+# Install
+`$ npm install emotional_awareness`
+
+# Usage
+Check how you feel
+`$ emotional_awareness`
+
+Example
+`? 今日の気分は？ …
+❯ 😄
+  🙂
+  🙁
+  😩
+
+✔ 今日の気分は？ · 😄
+なるほど！今日は😄な気分なんだね〜
+そんなあなたに今日の言葉を届けます〜👀
+
+No one will believe in you unless you do.
+`
+
+Save your feelings
+`$ emotional_awareness -s`
+
+Example
+`? 保存したい気分は？ …
+❯ 😄
+  🙂
+  🙁
+  😩
+`
+And also you can add some memo with your feelings, if you want.
+
+`
+  ✔ 保存したい気分は？ · 😩
+? 何か一言メモを残したい？ …
+❯ Yes
+  No
+
+✔ 何か一言メモを残したい？ · Yes
+OK！Yesなら1行メモを残そう！入力をどうぞ！
+`
--- index.js
+++ index.js
@@ -4,92 +4,96 @@ const argv = require('minimist')(process.argv.slice(2))
 require('date-utils')
 
 const dt = new Date()
-let format = dt.toFormat('YYYY年MM月DD日')
+const format = dt.toFormat('YYYY年MM月DD日')
 
 const Feeling = [
-    '😄',
-    '🙂',
-    '🙁',
-    '😩'
+  '😄',
+  '🙂',
+  '🙁',
+  '😩'
 ]
 
-
-const displayRandomPhrases = () => {
-    let words = fs.readFileSync(`quotes/a.txt`, 'utf-8')
-    let word = words.split('/')
-    let phrases = word.slice(0, 14)
-    let x = Math.floor(Math.random() * phrases.length)
-    setTimeout(() => console.log(phrases[x]), 1000)
-}
-
 const choseMyFeeling = () => {
-    (async () => {
-        const question1 = {
-            type: 'select',
-            name: 'feeling',
-            message: '今日の気分は？',
-            choices: Feeling
-        };
-        const answer1 = await Enquirer.prompt(question1)
-        console.log(`なるほど！今日は${answer1.feeling}な気分なんだね〜`)
-        console.log('そんなあなたに今日の言葉を届けます〜👀')
+  (async () => {
+    const question1 = {
+      type: 'select',
+      name: 'feeling',
+      message: '今日の気分は？',
+      choices: Feeling
+    }
+    const answer1 = await Enquirer.prompt(question1)
+    console.log(`なるほど！今日は${answer1.feeling}な気分なんだね〜`)
+    console.log('そんなあなたに今日の言葉を届けます〜👀')
 
-        if (`${answer1.feeling}` === '😄') {
-            displayRandomPhrases()
-        } else if (`${answer1.feeling}` === '🙂') {
-            let words = fs.readFileSync(`quotes/b.txt`, 'utf-8')
-            displayRandomPhrases()
-        } else if (`${answer1.feeling}` === '🙁') {
-            let words = fs.readFileSync(`quotes/c.txt`, 'utf-8')
-            displayRandomPhrases()
-        } else {
-            let words = fs.readFileSync(`quotes/d.txt`, 'utf-8')
-            displayRandomPhrases()
-        }
-    })();
+    if (`${answer1.feeling}` === '😄') {
+      const words = fs.readFileSync('quotes/a.txt', 'utf-8')
+      const word = words.split('/')
+      const phrases = word.slice(0, 14)
+      const x = Math.floor(Math.random() * phrases.length)
+      setTimeout(() => console.log(phrases[x]), 1000)
+    } else if (`${answer1.feeling}` === '🙂') {
+      const words = fs.readFileSync('quotes/b.txt', 'utf-8')
+      const word = words.split('/')
+      const phrases = word.slice(0, 14)
+      const x = Math.floor(Math.random() * phrases.length)
+      setTimeout(() => console.log(phrases[x]), 1000)
+    } else if (`${answer1.feeling}` === '🙁') {
+      const words = fs.readFileSync('quotes/c.txt', 'utf-8')
+      const word = words.split('/')
+      const phrases = word.slice(0, 14)
+      const x = Math.floor(Math.random() * phrases.length)
+      setTimeout(() => console.log(phrases[x]), 1000)
+    } else {
+      const words = fs.readFileSync('quotes/d.txt', 'utf-8')
+      const word = words.split('/')
+      const phrases = word.slice(0, 14)
+      const x = Math.floor(Math.random() * phrases.length)
+      setTimeout(() => console.log(phrases[x]), 1000)
+    }
+  })()
 }
 
 const saveMyFeeling = () => {
-    (async () => {
-        const question2 = {
-            type: 'select',
-            name: 'feeling',
-            message: '保存したい気分は？',
-            choices: Feeling
-        };
-        const answer2 = await Enquirer.prompt(question2);
-        fs.writeFileSync(`${format}.txt`, `${format}の気分: ${answer2.feeling}`)
+  (async () => {
+    const question2 = {
+      type: 'select',
+      name: 'feeling',
+      message: '保存したい気分は？',
+      choices: Feeling
+    }
+    const answer2 = await Enquirer.prompt(question2)
+    fs.writeFileSync(`Myfeeling/${format}.txt`, `${format}の気分: ${answer2.feeling}`)
 
-        const question3 = {
-            type: 'select',
-            name: 'confirmation',
-            message: '何か一言メモを残したい？',
-            choices: ['Yes', 'No']
-        };
-        const answer3 = await Enquirer.prompt(question3);
-        if (`${answer3.confirmation}` === 'Yes') {
-            console.log(`OK！${answer3.confirmation}なら1行メモを残そう！入力をどうぞ！`)
-            process.stdin.resume();
-            process.stdin.setEncoding('utf8')
+    const question3 = {
+      type: 'select',
+      name: 'confirmation',
+      message: '何か一言メモを残したい？',
+      choices: ['Yes', 'No']
+    }
+    const answer3 = await Enquirer.prompt(question3)
+    if (`${answer3.confirmation}` === 'Yes') {
+      console.log(`OK！${answer3.confirmation}なら1行メモを残そう！入力をどうぞ！`)
+      process.stdin.resume()
+      process.stdin.setEncoding('utf8')
 
-            let reader = require('readline').createInterface({
-                input: process.stdin,
-                output: process.stdout
-            });
-            reader.on('line', (input) => {
-                fs.appendFile(`${format}.txt`, '\n' + input, (err) => {
-                    if (err) throw err
-                    console.log(`『${input}』を保存しました`)
-                })
-            })
-        } else {
-            console.log('また今度〜👋')
-        }
-    })();
+      const reader = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+      })
+      reader.on('line', (input) => {
+        fs.appendFile(`Myfeeling/${format}.txt`, '\n' + input, (err) => {
+          if (err) throw err
+          console.log(`『${input}』を保存しました`)
+        })
+      })
+    } else {
+      console.log('また今度〜👋')
+    }
+  })()
 }
 
-if (argv.save) {
-    saveMyFeeling()
+if (argv.s) {
+  saveMyFeeling()
 } else {
-    choseMyFeeling()
+  choseMyFeeling()
 }
